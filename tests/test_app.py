a45184f3192cfcddcec5998eb8470124225084fd
dantonbertuol <dantonjb@gmail.com>
2025-07-23 22:46:09 -0300

    feat: pydantic e html return

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,15 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_root_html_deve_retornar_ok_e_html():
+    client = TestClient(app)
+
+    response = client.get("/html")
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1> Olá Mundo </h1>" in response.text
+    assert "<title> Nosso olá mundo!</title>" in response.text
+    assert "<html>" in response.text
+    assert "</html>" in response.text
