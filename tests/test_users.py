0a70fb7b6af637ea1b0e31bc33f3ad35c6d0268e
dantonbertuol <dantonjb@gmail.com>
2025-08-18 21:50:05 -0300

    refactor: Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

---
+++ tests/test_users.py
@@ -0,0 +1,130 @@
+from http import HTTPStatus
+
+from fast_api_crud.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def test_create_user_username_alredy_exists(client, user):
+    response = client.post(
+        "/users",
+        json={
+            "username": user.username,
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {"detail": "Username already exists"}
+
+
+def test_create_user_email_alredy_exists(client, user):
+    response = client.post(
+        "/users",
+        json={
+            "username": "alice",
+            "email": user.email,
+            "password": "secret",
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {"detail": "Email already exists"}
+
+
+def test_read_users(client):
+    response = client.get("/users")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get("/users/")
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_read_user(client, user):
+    response = client.get(f"/users/{user.id}")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": user.username,
+        "email": user.email,
+        "id": user.id,
+    }
+
+
+def test_read_user_not_found(client, user):
+    response = client.get("/users/999")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": user.id,
+    }
+
+
+def test_update_integrity_error(client, user, token):
+    # Criando um registro para "fausto"
+    client.post(
+        "/users",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "fausto",
+            "email": "fausto@example.com",
+            "password": "secret",
+        },
+    )
+
+    # Alterando o user.username das fixture para fausto
+    response_update = client.put(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "fausto",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {"detail": "Username or Email already exists"}
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted"}
